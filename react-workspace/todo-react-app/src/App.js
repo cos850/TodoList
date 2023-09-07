3fd3e174d36c885344e6f186c11bea218033a88f
hyeri <cos850@naver.com>
2023-09-07 23:00:29 +0900

    Meterial UI 패키지 추가

--- react-workspace/todo-react-app/src/App.js
+++ react-workspace/todo-react-app/src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
+  return (  // JSX: 자바스크립트 내부에서 HTML코드를 사용할 수 있는 문법 (Babel 라이브러리가 빌드 시 자바스크립트로 번역해줌)
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -22,4 +22,4 @@ function App() {
   );
 }
 
-export default App;
+export default App; // 해당 컴포넌트를 다른 컴포넌트에서 사용할 수 있음
--- react-workspace/todo-react-app/src/index.js
+++ react-workspace/todo-react-app/src/index.js
@@ -1,9 +1,25 @@
-import React from 'react';
+/**
+ * SPA (Single Page Application)
+ * : 한 번 웹 페이지를 로딩하면 사용자가 임의로 새로고침하지 않는 이상 페이지를 새로 로딩하지 않는 애플리케이션.
+ * 
+ * SPA 랜더링 순서
+ * 1. index.html 파싱&렌더링 (npm start 실행 시 하단에 bundle.js 생성. 해당 js 내부에 index.js 포함)
+ * 2. index.js 실행 
+ * 3. root 엘리먼트 하위 엘리먼트 렌더링
+ * 
+ */
+
+import React from 'react'; 
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './App';  // import를 사용해 App 컴포넌트 불러옴. "<컴포넌트이름 />" 와 같이 사용
 import reportWebVitals from './reportWebVitals';
 
+/**
+ * ReactDOM.render()
+ * : ReactDOM이 매개변수로 넘긴 컴포넌트를 root 엘리먼트에 랜더링함
+ * => 서버에 새 HTML 페이지를 요청하지 않고 자바스크립트가 동적으로 root 하위 엘리먼트를 수정 (클라이언트-사이드 랜더링)
+ */
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
